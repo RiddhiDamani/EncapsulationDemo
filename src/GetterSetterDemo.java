46dab2502ff109dc085ffadec5a20f6ad288bb01
Riddhi Damani <riddhidamani90@gmail.com>
2020-07-16 12:58:56 -0500

    Added Comments

--- src/GetterSetterDemo.java
+++ src/GetterSetterDemo.java
@@ -1,7 +1,17 @@
+/*
+ * In encapsulation, always use getter and setter method to access the variables of the other class. 
+ * Only way to access the other's class variables is thru the methods (i.e. getter & setter)
+ * Also, make sure to set the variables of this class are private and the methods used to access these 
+ * variables should be public. These means that you are binding your data with these methods. 
+ * And whenever you bind your data with methods - this process is known as encapsulation
+ * Encapsulation -> Binding Data with Methods
+ */
+
 class Student {
-	int rollNo;
-	String studentName;
+	private int rollNo;
+	private String studentName;
 	
+	//Getters and Setters
 	public int getRollNo() {
 		return rollNo;
 	}
